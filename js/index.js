12691ce6946670e1355689df4fe384064d4ffa69
thalesleite <thalesaleite@gmail.com>
2024-03-21 13:55:53 +0000

    change input-button to button and add event click to the .btn

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
-console.log("Calculator project running...")
+let results = ""
+const resultsDiv = document.querySelector(".results")
+
+document.querySelectorAll(".btn").forEach((button) => {
+  button.addEventListener("click", () => {
+    getValue(event.target.value)
+  })
+})
+
+const getValue = function (value) {
+  results += value
+  resultsDiv.textContent = results
+}
 
 const add = function (num1, num2) {
   return num1 + num2
@@ -14,4 +26,23 @@ const multiply = function (num1, num2) {
 
 const divide = function (num1, num2) {
   return num1 / num2
+}
+
+const clearResults = function () {
+  results = ""
+  resultsDiv.textContent = results
+}
+
+const operate = function () {
+  const operators = "*/-+"
+  const numbers = "1234567890"
+  const operatorsSlected = results
+    .split(" ")
+    .filter((operator) => operators.includes(operator))
+  const numbersSelected = results
+    .split(" ")
+    .filter((operator) => numbers.includes(operator))
+
+  console.log("operatorsSlected: ", operatorsSlected)
+  console.log("numbersSelected: ", numbersSelected)
 }
